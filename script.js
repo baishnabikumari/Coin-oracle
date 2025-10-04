639ed9ab894e97cec764ea5573d5bc1b774b0c54
baishnabikumari <baishnabi.5.06.2011@gmail.com>
2025-10-04 17:27:04 +0530

    Making a Coin Oracle website - Fortune For You !!

---
+++ script.js
@@ -0,0 +1,46 @@
+let coins = parseInt(localStorage.getItem("coins")) || 5;
+
+const balanceEl = document.getElementById("coin-balance");
+const spendBtn = document.getElementById("spend-coin");
+const getBin = document.getElementById("get-coin");
+const messageBox = document.getElementById("message-box");
+
+const message = [
+    "You will have a lucky day 🍀",
+  "A surprise is waiting for you 🎁",
+  "Happiness comes from within ✨",
+  "Your hard work will pay off soon 💼",
+  "Good news will come your way 📩",
+  "Someone is thinking about you 💭",
+  "Adventure awaits you 🌍",
+  "Smile, and the world smiles with you 😀",
+  "Expect the unexpected 🔮",
+  "Your energy attracts positive vibes 🌟"
+];
+
+function updateBalance() {
+    balanceEl.textContent = coins;
+    localStorage.setItem("coins", coins);
+}
+
+spendBtn.addEventListener("click", () => {
+    if (coins > 0) {
+        coins--;
+        updateBalance();
+
+        const randomIndex = Math.floor(Math.random() * message.length);
+        const fortune = message[randomIndex];
+
+        messageBox.textContent = fortune;
+    } else {
+        message.textContent = "Not enogh Coins!";
+    }
+});
+
+getBin.addEventListener("click", () => {
+    coins++;
+    updateBalance();
+    message.textContent = " You received a free coin!"
+});
+
+updateBalance();
